5576e144456298f8a5e89a3ce129b1b5984f5ce3
woong <k632ht@gmail.com>
2023-01-08 00:06:09 +0900

    add health check controller

---
+++ src/main/java/com/woong/projectmanager/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.woong.projectmanager.controller;
+
+import com.woong.projectmanager.common.Message;
+import com.woong.projectmanager.common.StatusEnum;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<Message> HealthChekc(){
+        Message message = new Message();
+        message.setStatus(StatusEnum.OK.OK);
+        return ResponseEntity.ok().body(message);
+    }
+}
